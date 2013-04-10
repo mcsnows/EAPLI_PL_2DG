c65fbe14a940eed76f306e5af4203cb4b1419f34
1020091-josenuno <outsider.outminder@gmail.com>
2013-04-10 14:17:08 +0100

    Adicção classe BaseUI e alteração correspondente ao método ExpenseRegisterUI - UC03-A-B

---
+++ eapli.ExpenseManager/src/Presentation/BaseUI.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentation;
+
+/**
+ *
+ * @author Outsider
+ */
+public abstract class BaseUI {
+    //protected abstract BaseController controller();
+    public abstract void header();
+    public abstract void run();
+    
+    public void show(){
+    header();
+    run();
+    showBalances();
+    }
+
+    public void showBalances(){
+         /*
+          NumberFormat n = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+         
+          BigDecimal wb = controller().getThisWeekExoenditure();
+          BigDecimal mb = controller().getThisMonthExoenditure();
+          double week = wb.doubleValue();
+          double month = mb.doubleValue();
+          System.out.println("Weekly expenditure"+ n.format(week));
+          System.out.println("Monthly expenditure" + n.format(month));
+          * 
+        */
+}
+}
