5e6d348eb1b7d3c2884710a1d3659b80ff3f5394
i100623 <i100623@PCI079.dei.isep.ipp.pt>
2013-04-16 17:06:11 +0100

    1100588 - ListPaymentMeans

--- eapli.ExpenseManager/src/Controllers/PaymentMeanController.java
+++ eapli.ExpenseManager/src/Controllers/PaymentMeanController.java
@@ -5,10 +5,12 @@ import Model.Check;
 import Model.CreditCard;
 import Model.DebitCard;
 import Model.Expense;
+import Model.PaymentMean;
 import Persistence.ExpenseRepository;
 import Persistence.PaymentMeansRepository;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 public class PaymentMeanController {
 
@@ -36,5 +38,11 @@ public class PaymentMeanController {
         DebitCard dc = new DebitCard(accountNumberDC, bankDC, validadeDC);
         
         repo.savePaymentMean(dc);
+    }
+    
+    public List<PaymentMean> listAllPaymentMeans(){
+        PaymentMeansRepository pmr = new  PaymentMeansRepository();
+        List<PaymentMean> list = pmr.getAllPaymentMean();
+        return list;
     }
 }
