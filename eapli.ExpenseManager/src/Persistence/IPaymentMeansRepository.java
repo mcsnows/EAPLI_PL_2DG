f8ad82dbbf3761951364351b406a6f15322805d5
i100623 <i100623@PCI079.dei.isep.ipp.pt>
2013-04-16 16:41:52 +0100

    1100623- PaymentMeansBootStraper

--- eapli.ExpenseManager/src/Persistence/IPaymentMeansRepository.java
+++ eapli.ExpenseManager/src/Persistence/IPaymentMeansRepository.java
@@ -1,16 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Persistence;
 
 import Model.PaymentMean;
 import java.util.List;
 
-/**
- *
- * @author José Nuno Loureiro
- */
+
 public interface IPaymentMeansRepository {
     void savePaymentMean(PaymentMean pm);
     List<PaymentMean> getAllPaymentMean();
