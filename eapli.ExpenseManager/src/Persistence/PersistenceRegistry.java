9089e348a167502d8d7e5464b68c18d2b4a6a40f
1020091-josenuno <outsider.outminder@gmail.com>
2013-04-16 14:48:32 +0100

    Criação das classes IncomeRegisterController, IncomeRepository Criação da interface IIncomeRepository Alteração da classe Persistence registry (Adição ao Singleton dos métodos relativos ao income) - UC09

--- eapli.ExpenseManager/src/Persistence/PersistenceRegistry.java
+++ eapli.ExpenseManager/src/Persistence/PersistenceRegistry.java
@@ -36,4 +36,12 @@ public class PersistenceRegistry {
         return new PaymentMeansRepository();
     }
     
+    public IIncomeRepository incomeRepository(){
+        return new IncomeRepository();
+    }
+    
+    public IIncomeTypeRepository incomeTypeRepository(){
+        return new IncomeTypeRepository();
+    }
+    
 }
